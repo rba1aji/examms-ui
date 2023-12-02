0fe303bbdc6ccefc06b0d4cc0a0f7907a800a643
Balaji_R <rjibalaji@gmail.com>
2023-12-02 23:13:49 +0530

    Add demo notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,27 @@ export default function Home() {
         <>
             <br />
             <div className="text-center h3 mt-2">Welcome to examms!</div>
+            <br />
+            <div style={{ fontSize: '14px', minHeight: '50vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'flex-row', gap: "100px" }}>
+                <div>
+                    <b> Demo Admin Login:</b>
+                    <br />
+                    username: balaji<br />
+                    password: balaji<br />
+                </div>
+                <div>
+                    <b> Demo Faculty Login:</b>
+                    <br />
+                    username: balaji<br />
+                    password: balaji<br />
+                </div>
+                <div>
+                    <b>Demo Student Login:</b>
+                    <br />
+                    username: 73772014108<br />
+                    password: 13/12/2003<br />
+                </div>
+            </div >
         </>
     )
 }
