ced104002035d8e98a46f5fba383997bc8a42d41
Balaji_R <developerrbalaji@gmail.com>
2024-10-06 01:32:13 +0530

    ServerUrl revert after pull

--- src/reducers/ServerUrl.js
+++ src/reducers/ServerUrl.js
@@ -1,2 +1,2 @@
-export const SPRINGBOOT_SERVER_URL = "https://demo-examms-service.onrender.com/api"
+export const SPRINGBOOT_SERVER_URL = "https://examms-springboot.onrender.com/api"
 // export const SPRINGBOOT_SERVER_URL = "http://localhost:7001/api"
